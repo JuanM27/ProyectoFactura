4c15d453703e47fe1c678d555d2c942fd8a00ac3
kuzafkto <kuzafkto@gmail.com>
2023-04-27 08:56:52 +0200

    chequeo en base a las restricciones de la BBDD con respecto al cliente

--- ProyectoFactura.java
+++ ProyectoFactura.java
@@ -32,18 +32,37 @@ class ProyectoFactura {
         case 1:
 
           System.out.print("Introduce el nombre del cliente: ");
-          String nombre = scanner.next();
+          String nombre = scanner.nextLine();
+          while(nombre.length==0){
+            System.out.println("Debe ingresar su apellido: ");
+            nombre=scanner.nextLine();
+          }
+          
+          System.out.print("Introduce el primer apellido: ");
+          String apellido = scanner.nextLine();
+          while(apellido.length==0){
+            System.out.println("Debe ingresar su apellido: ");
+            apellido=scanner.nextLine();
 
-          System.out.print("Introduce los apellidos: ");
-          String apellidos = scanner.next();
+          }
 
-          scanner.nextLine();
+          System.out.print("Introduce el segundo apellido (presione ENTER para omitir): ");
+          String apellido2 = scanner.nextLine();
 
-          System.out.print("Introduce el DNI: ");
-          String dni = scanner.next();
+          System.out.print("Introduce el DNI/NIE: ");
+          String dni = scanner.nextLine();
+          while(dni.length()!=9){
+            System.out.println("El DNI/NIE de telefono deben ser 9 dígitos, vuelva a         ingresarlo: ");
+            dni = scanner.nextLine();
+          }
 
           System.out.print("Introduce el número de teléfono: ");
-          int tel = scanner.nextInt();
+          String tel = scanner.nextInt();
+            while(s.matches(".*[a-z].*")||tel.length()!=9){
+              System.out.println("El numero de telefono deben ser 9 numeros, vuelva a         ingresarlo: ");
+              tel = scanner.nextInt();
+            }
+          
 
           Cliente cliente = new Cliente(nombre, apellidos, dni, tel);
 
@@ -71,7 +90,7 @@ class ProyectoFactura {
           Statement p = conexion.createStatement();
           ResultSet listado = p.executeQuery("SELECT * FROM cliente");
 
-          while (listado.next()) {
+          while (listado.nextLine()) {
             System.out.println(listado.getString("id") + (" ") + listado.getString("nombre"));
           }
 
